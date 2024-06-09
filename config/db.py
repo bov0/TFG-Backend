06aa1fd51c06699a1282e975e3302037831086e7
bov0 <ibay2002@gmail.com>
2024-06-09 21:49:12 +0200

    Conexion a Google Cloud SQL

--- config/db.py
+++ config/db.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine, MetaData
 
-MYSQLDB_HOST = "db4free.net"
-MYSQLDB_USUARIO = "ivan_fdez02"
-MYSQLDB_PASSWORD = "palomeras"
+#MYSQLDB_HOST = "db4free.net"
+#MYSQLDB_USUARIO = "ivan_fdez02"
+#MYSQLDB_PASSWORD = "palomeras"
+#MYSQLDB_BD = "iesdawivan02"
+
+#Conexión a Google Cloud SQL
+MYSQLDB_HOST = "34.175.31.241"
+MYSQLDB_USUARIO = "admin"
+MYSQLDB_PASSWORD = "admin1234"
 MYSQLDB_BD = "iesdawivan02"
 
 db_url = f"mysql+pymysql://{MYSQLDB_USUARIO}:{MYSQLDB_PASSWORD}@{MYSQLDB_HOST}/{MYSQLDB_BD}"
